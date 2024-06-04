b0b901e591d2d5e7e8a48e438fea3b939c33f665
mohmmad Smadi <mohmmadmms20@gmail.com>
2024-06-04 22:15:33 +0300

    Fire base

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 const adminMiddleware = require('../middleware/adminMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
-const { uploadCourseImage } = require('../utils/upload');
+const { handleCourseImageUpload } = require('../utils/upload');
 
 router.get('/courses', courseController.homepageForCourses);
-router.post('/courses', authMiddleware, adminMiddleware, uploadCourseImage, courseController.addNewCourse);
+router.post('/courses', authMiddleware, adminMiddleware, handleCourseImageUpload, courseController.addNewCourse);
 router.delete('/courses/:id', authMiddleware, adminMiddleware, courseController.deleteCourse);
 router.get('/courses/:id', courseController.getCourseById);
-router.put('/courses/:id', authMiddleware, adminMiddleware, uploadCourseImage, courseController.updateCourse);
+router.put('/courses/:id', authMiddleware, adminMiddleware, handleCourseImageUpload, courseController.updateCourse);
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { uploadProfileImage } = require('../utils/upload');
+const { handleProfileImageUpload } = require('../utils/upload');
 
 const {
   signup,
@@ -13,11 +13,11 @@ const {
 const adminMiddleware = require('../middleware/adminMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/signup', uploadProfileImage, signup);
+router.post('/signup', handleProfileImageUpload, signup);
 router.post('/login', login);
 router.get('/my-profile', authMiddleware, profile);
 router.post('/signout', signout);
-router.put('/edit-profile/:id', authMiddleware, uploadProfileImage, editProfile);
+router.put('/edit-profile/:id', authMiddleware, handleProfileImageUpload, editProfile);
 router.delete('/delete-account/', authMiddleware, deleteAccount);
 
 module.exports = router;
